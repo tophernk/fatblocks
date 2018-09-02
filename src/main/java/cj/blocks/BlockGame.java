1054a4df634e5a01f1c8e9134901996f4eeb0072
tophernk <topher.nk@googlemail.com>
2018-09-02 13:44:10 +0200

    removed inner class

--- src/main/java/cj/blocks/BlockGame.java
+++ src/main/java/cj/blocks/BlockGame.java
@@ -165,44 +165,4 @@ public class BlockGame {
             currentPiece_x--;
         }
     }
-
-    private static class Coordinates {
-        private int x;
-        private int y;
-
-        public Coordinates(int x, int y) {
-            this.x = x;
-            this.y = y;
-        }
-
-        public int getX() {
-            return x;
-        }
-
-        public int getY() {
-            return y;
-        }
-
-        public static Coordinates determineNextBlockCoordinates(Block block, int previousBlockY, int previousBlockX) {
-            int y = previousBlockY;
-            int x = previousBlockX;
-            if (block.direction != null) {
-                switch (block.direction) {
-                    case UP:
-                        y--;
-                        break;
-                    case DOWN:
-                        y++;
-                        break;
-                    case LEFT:
-                        x--;
-                        break;
-                    case RIGHT:
-                        x++;
-                        break;
-                }
-            }
-            return new Coordinates(x, y);
-        }
-    }
 }
